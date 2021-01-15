aae7446c8dc2f19ffc29ce709df5c6cba788887f
atacksuqui8 <josegallard88@gmail.com>
2021-01-15 05:00:05 -0800

    SE MODIFICA EL SCRIPT DE BASE DE DATOS CON LA CORRECIONES DETECTADAS POR ZOE Y SE LE AÑADE UNA TABLA DE DONACION PARA GUARDAR LA INFORMACION

--- ENTREGABLES/SQL/script hack a boss.sql
+++ ENTREGABLES/SQL/script hack a boss.sql
@@ -8,9 +8,13 @@ create table usuario (
   idusuario int(6) not null primary key,
   admin boolean not null default false,
   nombreusuario varchar(20) not null,
+  nombreperfilusuario varchar(20) not null unique,
+  contraseña varchar (40) not null,
   apel1 varchar (40) not null,
   apel2 varchar (40) not null,
-  codFoto varchar (10) not null
+  email varchar(40) not null,
+  codFoto varchar (20) null
+  
 )
 ;
 
@@ -49,9 +53,21 @@ Create table reserva (
     idlibro int(6) not null,
     fechareserva datetime,
     fechadevolucion datetime,
+	rating int null,
     primary key (idusuario,idlibro,fechareserva),
 	constraint `fk_UsuarioReserva` FOREIGN KEY (`idusuario` ) REFERENCES `proyectoFinalHACKABOSS`.`usuario` (`idusuario` ),
     constraint `fk_LibroReserva` FOREIGN KEY (`idlibro` ) REFERENCES `proyectoFinalHACKABOSS`.`libro` (`idlibro`) on delete restrict
+);
+
+create table donacion (
+	idusuario int(6) not null,
+    idlibro int (6) not null,
+    fechadonacion datetime,
+	revisado boolean not null default false,
+    primary key (idusuario,idlibro),
+    constraint `fk_UsuarioDonacion` FOREIGN KEY (`idusuario` ) REFERENCES `proyectoFinalHACKABOSS`.`usuario` (`idusuario` ),
+    constraint `fk_LibroDonacion` FOREIGN KEY (`idlibro` ) REFERENCES `proyectoFinalHACKABOSS`.`libro` (`idlibro`) on delete restrict
+    
 );
 
 
@@ -86,9 +102,9 @@ create table detalle (
   constraint `fk_idFactura` FOREIGN KEY (`idfactura` ) REFERENCES `proyectoFinalHACKABOSS`.`factura` (`idfactura` ) on delete restrict
 );
 
-INSERT INTO proyectoFinalHACKABOSS.usuario values (1,true,"Jose","Lopez","Fernandez","cod1");
-INSERT INTO proyectoFinalHACKABOSS.usuario values (2,true,"Barbara","Imbernon","loquesea","cod2");
-INSERT INTO proyectoFinalHACKABOSS.usuario values (3,true,"Jose","Santos","Iglesias","cod3");
+INSERT INTO proyectoFinalHACKABOSS.usuario values (1,true,"Jose","JOSELF","abc123.","Lopez","Fernandez","josegallard88@gmail.com","cod1");
+INSERT INTO proyectoFinalHACKABOSS.usuario values (2,true,"Barbara","BARI","abc123.","Imbernon","loquesea","josegallard88@gmail.com","cod2");
+INSERT INTO proyectoFinalHACKABOSS.usuario values (3,true,"Jose","JOSESI","abc123.","Santos","Iglesias","josegallard88@gmail.com","cod3");
 INSERT INTO proyectoFinalHACKABOSS.categoria values (1,"Terror","Categoria destinada a los libros de terror");
 INSERT INTO proyectoFinalHACKABOSS.categoria values (2,"Fantasia","Categoria destinada a los libros de fantasia");
 INSERT INTO proyectoFinalHACKABOSS.categoria values (3,"Aventura","Categoria destinada a los libros de aventura");
@@ -98,13 +114,14 @@ INSERT INTO proyectoFinalHACKABOSS.autor values (1,1,"Miguel","Cervantes","Saave
 INSERT INTO proyectoFinalHACKABOSS.autor values (2,2,"Joanne","Rowling","Rowling");
 INSERT INTO proyectoFinalHACKABOSS.tarjeta values (1,"4000 1234 5673 9875",1,"04/21",343);
 
-INSERT INTO proyectoFinalHACKABOSS.reserva values (1,1,"1996-07-10",null);
+INSERT INTO proyectoFinalHACKABOSS.reserva values (1,1,"1996-07-10",null,5);
 
 INSERT INTO `proyectofinalhackaboss`.`factura` (`idfactura`, `idusuario`, `fecha`, `iva`) VALUES ('1', '1', '2021-01-04', '20.00');
 
 INSERT INTO `proyectofinalhackaboss`.`detalle` (`idfactura`, `iddetalle`, `idlibro`, `precio`) VALUES ('1', '1', '1', '20.0');
 INSERT INTO `proyectofinalhackaboss`.`detalle` (`idfactura`, `iddetalle`, `idlibro`, `precio`) VALUES ('1', '2', '1', '30.0');
 INSERT INTO `proyectofinalhackaboss`.`detalle` (`idfactura`, `iddetalle`, `idlibro`, `precio`) VALUES ('1', '3', '1', '50.0');
+
 
 
 
